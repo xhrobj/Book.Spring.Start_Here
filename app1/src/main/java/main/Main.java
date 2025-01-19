99d688246918214744a291a464c54c1788c54d5d
M <m1@mishe1.ru>
2025-01-19 12:12:09 +0300

    Добавлен экземпляр контекста Spring

--- app1/src/main/java/main/Main.java
+++ app1/src/main/java/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
 import model.Parrot;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(); // Создание экземпляра контекста Spring
+
         Parrot parrot = new Parrot();
         parrot.setName("Jameson");
 
