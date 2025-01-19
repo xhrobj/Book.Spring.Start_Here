1f49eee7b17c507b379e94f95119cafead434ee3
M <m1@mishe1.ru>
2025-01-19 18:17:43 +0300

    Решение циклической зависимости через @Autowired сеттера

--- app1/src/main/java/model/Parrot.java
+++ app1/src/main/java/model/Parrot.java
@@ -1,15 +1,13 @@
 package model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Parrot {
     private String name = "Koko";
-    private Pirate owner;
 
-    public Parrot(Pirate owner) {
-        this.owner = owner;
-    }
+    private Pirate owner;
 
     public String getName() {
         return name;
@@ -17,6 +15,15 @@ public class Parrot {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Pirate getOwner() {
+        return owner;
+    }
+
+    @Autowired
+    public void setOwner(Pirate owner) {
+        this.owner = owner;
     }
 
     @Override
--- app1/src/main/java/model/Pirate.java
+++ app1/src/main/java/model/Pirate.java
@@ -1,6 +1,5 @@
 package model;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
